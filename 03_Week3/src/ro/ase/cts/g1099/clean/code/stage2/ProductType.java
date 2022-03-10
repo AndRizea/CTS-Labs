3dbadb342568631ab1711d2b9e7876cc988e181a
Andreea Rizea <andreearizea18@yahoo.com>
2022-03-10 21:25:54 +0200

    Lab3

---
+++ 03_Week3/src/ro/ase/cts/g1099/clean/code/stage2/ProductType.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.g1099.clean.code.stage2;
+
+public enum ProductType {
+
+	NEW(1, 0), DISCOUNT(2, 0.15f), SALES(3, 0.25f), FINAL_SALES(3, 0.35f);
+
+	int id;
+	float discountValue;
+
+	// only private constructors in enums
+	private ProductType(int id, float discountValue) {
+		this.id = id;
+		this.discountValue = discountValue;
+	}
+
+	float getDiscount() {
+		return this.discountValue;
+	}
+}
