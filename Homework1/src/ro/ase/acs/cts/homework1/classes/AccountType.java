5177d11cfa32ba2d21ad693c6a34781796f60720
AndreeaRizea <andreearizea18@yahoo.com>
2022-05-05 12:27:58 +0300

    hw1 restored

---
+++ Homework1/src/ro/ase/acs/cts/homework1/classes/AccountType.java
@@ -0,0 +1,6 @@
+package ro.ase.acs.cts.homework1.classes;
+
+public enum AccountType {
+
+	STANDARD, BUDGET, PREMIUM, SUPER_PREMIUM
+}
---
+++ Homework1/src/ro/ase/acs/cts/homework1/exceptions/MissingLoanValueException.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.cts.homework1.exceptions;
+
+public class MissingLoanValueException extends RuntimeException {
+
+}
